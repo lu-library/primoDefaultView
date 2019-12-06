8d597552882f4f3b0f7ef97fd4b81f67919dfbdc
Jason <57146833+lul-Jason@users.noreply.github.com>
2019-12-06 15:00:29 -0500

    Update custom.js

--- 01OCUL_LHD-LHD_DEFAULT/js/custom.js
+++ 01OCUL_LHD-LHD_DEFAULT/js/custom.js
@@ -7,7 +7,7 @@
 	/*----------below is the code for libraryh3lp -----------*/
   var s = document.createElement("script");
   s.id = "localScript";
-  s.src = "https://ca.libraryh3lp.com/js/libraryh3lp.js?776";
+  s.src = "https://ca.libraryh3lp.com/js/libraryh3lp.js?621";
   document.head.appendChild(s);
 	/*----------End of library3lp --------------------------*/
 })();
